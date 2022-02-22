2ac439e31a8aa47910cf3c0c8192e7bac866ecdc
reyesdiego90 <reyesdiego90@gmail.com>
2022-02-21 18:01:21 -0600

    Customer entity

--- src/main/java/com/dsu/bookborrowing/entity/Customer.java
+++ src/main/java/com/dsu/bookborrowing/entity/Customer.java
@@ -1,4 +1,28 @@
 package com.dsu.bookborrowing.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+@Entity
 public class Customer {
+    @Id
+    private Integer customer_id;
+
+    private String username;
+
+    private String name;
+
+    private Integer rol;
+
+    private Integer cellphone;
 }
