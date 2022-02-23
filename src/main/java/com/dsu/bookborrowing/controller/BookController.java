42489b78cbb2983897293f5df1517cd52f65a6d5
Fernando Pensamiento <fernando.pensamiento@telusinternational.com>
2022-02-23 11:31:54 -0600

    adding log4j

--- src/main/java/com/dsu/bookborrowing/controller/BookController.java
+++ src/main/java/com/dsu/bookborrowing/controller/BookController.java
@@ -1,8 +1,11 @@
 package com.dsu.bookborrowing.controller;
 
 
+import com.dsu.bookborrowing.BookBorrowingApplication;
 import com.dsu.bookborrowing.entity.Book;
 import com.dsu.bookborrowing.service.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,11 +14,15 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/v1/Book")
 public class BookController {
+    private static final Logger logger = LoggerFactory.getLogger(BookBorrowingApplication.class);
+
     @Autowired
     BookService bookService;
 
     @GetMapping
     public ArrayList<Book>  getBooks(){
+        logger.info("entered to  getBooks");
+
         return bookService.getBooks();
     }
 
